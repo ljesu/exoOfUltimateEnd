52351814a8bcc129a0085e0efa9cd9bfa80502cb
ljesu <twitch.tv@laposte.net>
2017-07-04 15:26:29 +0200

    reussi lol

--- mystrcmp.c
+++ mystrcmp.c
@@ -1,21 +1,17 @@
-int mystrcmp(const char str1[], const char str2[])
+int mystrcmp ( const char str1[], const char str2[])
 {
+	int i;
+	i = 0;
 
-	int i=0;
-	while (str1[i]!='\0')
+	while ( str1[i] == str2[i] )
 	{
-	
-	i=i+1;
+		if ( str1[i] == 0 || str2[i] == 0 )
+		{
+			return(0);
+		}
+		i = i+1;
 	}
-
-
-	int e=0;
-	while (str2[e]!='\0')
-	{
-	e=e+1;
-	}
-
-return(i-e);
+	int x = str1[i];
+	int y = str2[i];
+	return(x-y);
 }
-
-
